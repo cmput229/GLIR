f2b4ec01842ff6a31da43ac6b747494fda65bbe9
TaylorZowtuk <zowtukt@gmail.com>
2019-06-20 12:01:15 -0600

    Reformatted

--- runColor.sh
+++ runColor.sh
@@ -1,5 +1,13 @@
+# runColor.sh
+# Author:   Taylor Zowtuk
+# Date:     June 20, 2019
+#
+# Useage:   ./runColor.sh
+#
+# Runs the colorTest.s by concatenating the colorTest.s file to the GLIR.s file.
+
 rm -f test.s
 cat colorTest.s > test.s
 cat GLIR.s >> test.s
-rars test.s nc
+rars sm nc me test.s
 rm -f test.s
