b6ad8d1c529895e9c03d2951bef47ac9fd9acd06
TaylorZowtuk <zowtukt@gmail.com>
2019-08-29 16:12:57 -0600

    Added functions to replace print and wait syscalls

---
+++ src/Printff.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+void _printff(char* str)
+{
+    // Prints the string pointed to by str
+    // Explicitly flushes stdout b/c otherwise the board seems to buffer
+    printf(str);
+    fflush(stdout);
+    return;
+}
